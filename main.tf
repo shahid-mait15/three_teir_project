7efafd182ed702dd0889696b5fa6c70609897f77
shahid-mait15 <118314132+shahid-mait15@users.noreply.github.com>
2023-01-20 20:19:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,22 @@ module "VPC" {
   private_data_subnet_az3_cidr = var.private_data_subnet_az3_cidr
 }
 
+#create nat gateway
+module "nat_gateway" {
+  source = "../Project Modules/nat-gateway"
+  
+  vpc_id = module.VPC.vpc_id
+  internet_gateway = module.VPC.internet_gateway
+
+  public_subnet_az1_id = module.VPC.public_subnet_az1_id
+  public_subnet_az2_id = module.VPC.public_subnet_az2_id
+  public_subnet_az3_id = module.VPC.public_subnet_az3_id
+
+  private_app_subnet_az1_id = module.VPC.private_app_subnet_az1_id
+  private_app_subnet_az2_id = module.VPC.private_app_subnet_az2_id
+  private_app_subnet_az3_id = module.VPC.private_app_subnet_az3_id
+
+  private_data_subnet_az1_id = module.VPC.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.VPC.private_data_subnet_az2_id
+  private_data_subnet_az3_id = module.VPC.private_data_subnet_az3_id
+}
